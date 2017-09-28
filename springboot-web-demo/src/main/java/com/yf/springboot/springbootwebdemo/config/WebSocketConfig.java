1088fb3306360d191bdc0d19338113c0384c2a44
yf482 <330410708@qq.com>
2017-09-28 23:25:06 +0800

    spring boot

--- springboot-web-demo/src/main/java/com/yf/springboot/springbootwebdemo/config/WebSocketConfig.java
+++ springboot-web-demo/src/main/java/com/yf/springboot/springbootwebdemo/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.yf.springboot.springbootwebdemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,6 +12,13 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/endpointWisely").withSockJS();
 
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        super.configureMessageBroker(registry);
+        registry.enableSimpleBroker("topic");
     }
 }
